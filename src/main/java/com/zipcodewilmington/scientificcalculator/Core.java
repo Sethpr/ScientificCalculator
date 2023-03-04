166b7ebd7df06902dad60609154b3dc42bb986da
Seth <seth@Zipcoders-MBP.home>
2023-03-04 12:34:26 -0500

    added extra functionality and fixed typo

--- src/main/java/com/zipcodewilmington/scientificcalculator/Core.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Core.java
@@ -11,15 +11,35 @@ public class Core{
         return state;
     }
 
+    public void setState(double x){
+        state = x;
+    }
+
     public void clearDisplay(){
         System.out.println();
+    }
+
+    public double add(double x, double y){
+        return x + y;
+    }
+
+    public double subtract(double x, double y){
+        return x - y;
+    }
+
+    public double multiply(double x, double y){
+        return x * y;
+    }
+
+    public double divide(double x, double y){
+        return x/y;
     }
 
     public double square(double x){
         return x*x;
     }
 
-    public double exponenet(double x, double y){
+    public double exponent(double x, double y){
         return Math.pow(x, y);
     }
 
