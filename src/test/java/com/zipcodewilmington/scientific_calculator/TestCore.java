50eb094f4fb7410200d40293e4e5da969ee4ec7a
Jacob C <jacobc@Zipcoders-MacBook-Pro.local>
2023-03-05 15:48:22 -0500

    almost done tests

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestCore.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestCore.java
@@ -13,82 +13,82 @@ public class TestCore {
     public void addTest1(){
         double expected = 0;
         double actual = core.add(-1.1, 1.1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void subtractTest1(){
         double expected = 2.2;
         double actual = core.subtract(1.1, -1.1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void multiplyTest1(){
         double expected = 0.01;
         double actual = core.multiply(-0.1, -0.1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void divideTest1(){
         double expected = 1;
         double actual = core.divide(-0.1, -0.1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void squareTest1(){
         double expected = 1;
         double actual = core.square(-1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void squareTest2(){
         double expected = 0.01;
         double actual = core.square(0.1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void squareTest3(){
         double expected = 0;
         double actual = core.square(0);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
 
     @Test
     public void exponentTest1() {
         double expected = 1.0;
         double actual = core.exponent(0.0,0.0);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
 
     @Test
     public void squareRootTest1() {
         double expected = 1.0;
         double actual = core.squareRoot(1.0);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void squareRootTest2() {
         double expected = 0.316227;
         double actual = core.squareRoot(0.1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
         System.out.println(core.squareRoot(0.1));
     }
     @Test
     public void inverseTest1() {
         double expected = 0.1;
         double actual = core.inverse(10.0);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void switchSignsTest1() {
         double expected = -1.0;
         double actual = core.switchSign(1.0);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void switchSignsTest2() {
         double expected = 0.0;
         double actual = core.switchSign(0.0);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
 }
 
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestScientific.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestScientific.java
@@ -11,77 +11,77 @@ public class TestScientific {
     public void setup() {scientific = new Scientific();}
     @Test
     public void getSineTest1(){
-        double expected = 0.01745;
+        double expected = 0.841470;
         double actual = scientific.getSine(1);
         Assert.assertEquals(expected, actual, 0.00001);
     }
     @Test
     public void getCosineTest1(){
-        double expected = 0.99;
-        double actual = scientific.getCosine(5);
-        Assert.assertEquals(expected, actual, 0.02);
+        double expected = 0.540302;
+        double actual = scientific.getCosine(1);
+        Assert.assertEquals(expected, actual, 0.000001);
     }
 
     @Test
     public void getTangentTest1(){
-        double expected = 1.5574;
+        double expected = 1.557407;
         double actual = scientific.getTangent(1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.000001);
     }
     @Test
     public void getInverseSineTest1(){
-        double expected = 1.57;
+        double expected = 1.570796;
         double actual = scientific.getInverseSine(1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.000001);
     }
 
     @Test
     public void getInverseCosineTest1(){
         double expected = 0;
         double actual = scientific.getInverseCosine(1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
 
     @Test
     public void getInverseTangentTest1(){
         double expected = .785398;
         double actual = scientific.getInverseTangent(1);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void switchUnitsModeTest1(){
         String expected = String.valueOf(.84);
         String actual = scientific.switchUnitsMode(String.valueOf(1));
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void getLog1(){
         double expected = 0.69897;
         double actual = scientific.getLog(5);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void getInverseLog1(){
-        double expected = 1000000;
-        double actual = scientific.getInverseLog(5);
-        Assert.assertEquals(expected, actual, 0.02);
+        double expected = 10;
+        double actual = scientific.getInverseLog(1);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void getNaturalLog1(){
         double expected = 1.60943;
         double actual = scientific.getNaturalLog(5);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void getInverseNatLog1(){
         double expected = 148.4131591;
         double actual = scientific.getInverseNatLog(5);
-        Assert.assertEquals(expected, actual, 0.02);
+        Assert.assertEquals(expected, actual, 0.01);
     }
     @Test
     public void factorial1(){
-        double expected = 24;
-        double actual = scientific.factorial(-4);
-        Assert.assertEquals(expected, actual, 0.02);
+        double expected = -6;
+        double actual = scientific.factorial(-3);
+        Assert.assertEquals(expected, actual, 0.01);
     }
 }
